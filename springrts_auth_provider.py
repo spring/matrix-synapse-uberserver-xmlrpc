02d14158a641ce05f84d4422ece6fda604d0f66c
TurBoss <j.l.toledano.l@gmail.com>
2018-08-05 12:23:21 +0200

    set Display name

--- springrts_auth_provider.py
+++ springrts_auth_provider.py
@@ -58,8 +58,8 @@ class SpringRTSAuthProvider(object):
 
             matrix_account, access_token = (yield self.account_handler.register(localpart=matrix_id))
 
-            # store = yield self.account_handler.hs.get_profile_handler().store
-            # yield store.set_profile_displayname(user_id, username)
+            store = yield self.account_handler.hs.get_profile_handler().store
+            yield store.set_profile_displayname(matrix_id, username)
 
             registration = True
 
